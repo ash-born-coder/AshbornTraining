cc8b44482f1fd122b5c7f150b12600d2c901b37a
Chanuka Ashan <chanukagunawardena63@gmail.com>
2025-01-14 20:43:58 +0100

    Add files via upload

---
+++ exercise6(teamplayers).c
@@ -0,0 +1,27 @@
+/*
+Name - Chanuka Ashan GUnawardena
+Module - Software COnstruction 1 (C programming)
+
+Description - Pupils are divided into eight groups for an athletic competiotion. This program determines the number of pupils in 
+each team and how many pupils remained left over with two classes with 24 pupils and the other two with 25 and 25.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+
+	int classA = 24;
+	int classB = 24;
+	int classC = 25;
+	int classD = 26;
+	int totalStudents = classA + classB + classC + classD;  //getting the summation of total number of students
+	int teamNum = 8;
+	int studentsInGroup = totalStudents / teamNum;  //dividing the total number of student by the number of groups
+	int studentsRemaining = totalStudents % teamNum;
+
+	printf("The number of pupils in each team: %d\n", studentsInGroup);
+	printf("The number of pupils remaining: \%d", studentsRemaining);
+
+	return 0;
+}
