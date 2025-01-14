bdfbac29413cf96185718e0ad43be0976cc7fe10
ash-born-coder <chanukagunawardena63@gmail.com>
2025-01-14 20:10:50 +0100

    Add files via upload

---
+++ exercise5(binaryweights).c
@@ -0,0 +1,23 @@
+/*
+Name - Chanuka Ashan GUnawardena
+Module - Software COnstruction 1 (C programming)
+
+Description - This program calculates the weights 2^n of binary digits for n < 8 and prints them to the console.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+
+	int degree = 0;
+
+	//the for loop to iterate the power until 8
+	for (int i = 0; i < 8; i++)
+	{
+		int degree = (int)pow(2, i);
+		printf("The value of 2 with the power %d is %d\n", i, degree);
+	}
+
+	return 0;
+}
