12a1b0157481eff282b69aa80f0e5275880f9a3e
Chanuka Ashan <chanukagunawardena63@gmail.com>
2025-02-01 10:27:14 +0100

    Add files via upload

---
+++ playground.c
@@ -0,0 +1,37 @@
+/*
+Name - Chanuka Ashan Gunawardena
+Module - Software Construction 1 (C programming)
+
+Description - This program computes the Fibonacci sequence
+*/
+// - are the comments
+/*are the modification done to the algorithm*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+double sin();
+double principleValue();
+
+int main() {
+
+	int kTerms = 0;
+	int xTerms = 0;
+	
+	printf("Series Expansion for sin (x * PI) with k > 0 terms\n");
+	
+
+
+	return 0;
+}
+
+double sin()
+{
+	return 0.0;
+}
+
+double principleValue()
+{
+	return 0.0;
+}
